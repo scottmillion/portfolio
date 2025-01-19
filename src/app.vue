fbf9d4cc5137d33656e69af067e1a5a127762513
Scott Million <scott.million@nttdata.com>
2025-01-19 14:51:56 -0500

    InitialSetup-Tailwind-Typography-ContainerQueries-And-DaisyuiComponents

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,29 @@
-<script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const apiBaseUrlTest = runtimeConfig.public.apiBase
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <div class="mb-4 border-b border-b-gray-300">
-      <div>
-        <button @click="navigateTo('/')">Navigate to /</button>
-
-        <button @click="navigateTo('/playground')">Navigate to /playground</button>
-      </div>
-      <div>
-        clientSide:
-        {{ apiBaseUrlTest }}
+  <div class="@container relative flex h-[100dvh] w-full flex-col items-center overflow-auto">
+    <div class="mb-6 flex items-center justify-center border-b border-b-gray-200 py-2">
+      <div class="flex gap-x-4">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/playground">Playground</NuxtLink>
       </div>
     </div>
-    <NuxtPage />
+    <!-- TODO: make default and other layouts -->
+    <div
+      class="bg-neutral @4xl:w-[800px] mb-6 flex h-full w-full flex-col overflow-scroll rounded p-4"
+    >
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.2rem);
+}
+</style>
--- src/pages/playground/index.vue
+++ src/pages/playground/index.vue
@@ -1,16 +1,61 @@
 <script setup lang="ts">
 import { useAppStore } from "@/stores/app"
+import { useDebounceFn } from "@vueuse/core"
 
 const appStore = useAppStore()
 const runtimeConfig = useRuntimeConfig()
+
+const DEBOUNCE_WAIT = 500
+const toggleTestBooleanDebounce = useDebounceFn(() => {
+  appStore.toggleTestBoolean()
+}, DEBOUNCE_WAIT)
 </script>
 
 <template>
   <div>
-    <h1>Playground</h1>
+    <article class="prose prose-sm lg:prose-base xl:prose-lg w-full">
+      <h1>Playground</h1>
 
-    <div>Client variable apiBase: {{ runtimeConfig.public.apiBase }}</div>
-    <div>Test Pinia Store Value: {{ appStore.testBoolean }}</div>
-    <button @click="appStore.toggleTestBoolean()">Toggle Test Boolean</button>
+      <section>
+        <h2>Runtime Config</h2>
+        <p>Client variable apiBase: {{ runtimeConfig.public.apiBase }}</p>
+      </section>
+
+      <section>
+        <h2>Buttons & Colors</h2>
+        <div class="flex flex-wrap gap-2">
+          <button class="btn outline outline-1 outline-gray-500">Default w/ Outline</button>
+          <button class="btn btn-primary">Primary</button>
+          <button class="btn btn-secondary">Secondary</button>
+          <button class="btn btn-accent">Accent</button>
+          <button class="btn btn-info">Info</button>
+          <button class="btn btn-neutral outline outline-1 outline-gray-500">
+            Neutral w/ Outline
+          </button>
+          <button class="btn btn-success">Success</button>
+          <button class="btn btn-warning">Warning</button>
+          <button class="btn btn-error">Error</button>
+        </div>
+      </section>
+
+      <section>
+        <h2>Debounce & Pinia</h2>
+        <p>Test Pinia Store Value: {{ appStore.testBoolean }}</p>
+        <div class="flex w-full flex-col justify-center gap-y-2">
+          <button
+            class="btn btn-secondary max-w-[300px]"
+            @click="appStore.toggleTestBoolean"
+          >
+            Toggle Test Boolean
+          </button>
+          <button
+            class="btn btn-primary max-w-[300px]"
+            @click="toggleTestBooleanDebounce"
+          >
+            Toggle Test Boolean Debounce
+          </button>
+        </div>
+      </section>
+    </article>
   </div>
 </template>
